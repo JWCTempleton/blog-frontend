578dfd67ce78dd6c5c200f8a72d0ce7b1813a67d
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-16 18:31:01 -0700

    Add User state to hold logged in users, login form for users to enter their usernames and passwords for verification

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
+
 import { useEffect, useState } from "react";
 
 function App() {
   const [allBlogs, setAllBlogs] = useState([]);
   const [newTitle, setNewTitle] = useState("");
   const [newUrl, setNewUrl] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((response) => {
@@ -26,6 +31,34 @@ function App() {
     setNewUrl(event.target.value);
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      // setErrorMessage("Wrong credentials");
+      // setTimeout(() => {
+      //   setErrorMessage(null);
+      // }, 5000);
+      console.log("error", exception);
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    window.localStorage.removeItem("loggedNoteappUser");
+  };
+
   const blogForm = () => (
     <form onSubmit={addBlog}>
       <div>
@@ -41,10 +74,41 @@ function App() {
     </form>
   );
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
   return (
     <div className="App">
       <h1>Blogs</h1>
-      {blogForm()}
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>Logout</button>
+          {blogForm()}
+        </div>
+      )}{" "}
       {allBlogs.map((blog) => {
         return (
           <div key={blog.title} style={{ margin: "3rem" }}>
