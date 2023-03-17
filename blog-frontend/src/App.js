b32892128c5281359804c24fa0ebe318880a9ecc
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-16 18:03:42 -0700

    Add state to hold blog title and url when submitting a new blog, form to handle state and eventually submission of form data to mongodb (currently not set up)

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [allBlogs, setAllBlogs] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newUrl, setNewUrl] = useState("");
 
   useEffect(() => {
     blogService.getAll().then((response) => {
@@ -11,10 +13,38 @@ function App() {
     });
   }, []);
 
-  console.log("blogs", allBlogs);
+  const addBlog = (event) => {
+    event.preventDefault();
+    console.log(newTitle, newUrl);
+  };
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value);
+  };
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value);
+  };
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        Title:{" "}
+        <input type="text" value={newTitle} onChange={handleTitleChange} />
+      </div>
+      <div>
+        URL: <input value={newUrl} onChange={handleUrlChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+
   return (
     <div className="App">
       <h1>Blogs</h1>
+      {blogForm()}
       {allBlogs.map((blog) => {
         return (
           <div key={blog.title} style={{ margin: "3rem" }}>
