2c4d487f7ffd58d4b34e9f6c586a25cf4dc4008e
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-20 19:09:26 -0700

    Add button that opens and closes Blog post card to show more or less information, controlled through state in Blog card component

--- blog-frontend/src/components/BlogCard.js
+++ blog-frontend/src/components/BlogCard.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const styles = {
   margin: "2rem auto",
   padding: "2rem",
@@ -6,15 +8,36 @@ const styles = {
   width: "16rem",
 };
 
-const BlogCard = ({ blog }) => (
-  <div style={styles}>
-    <p>{blog.title}</p>
-    <p>{blog.author}</p>
-    <p>
-      <a href={blog.url}>{blog.url}</a>
-    </p>
-    <p>{blog.likes}</p>
-  </div>
-);
+const BlogCard = ({ blog }) => {
+  const [cardOpen, setCardOpen] = useState(false);
+
+  const displayCard = () => {
+    if (cardOpen === true) {
+      return (
+        <div style={styles}>
+          <p>
+            {blog.title}{" "}
+            <button onClick={() => setCardOpen(false)}>Hide</button>
+          </p>
+          <p>{blog.author}</p>
+          <p>
+            <a href={blog.url}>{blog.url}</a>
+          </p>
+          <p>{blog.likes}</p>
+        </div>
+      );
+    } else if (cardOpen === false) {
+      return (
+        <div style={styles}>
+          <p>
+            {blog.title}, {blog.author}{" "}
+            <button onClick={() => setCardOpen(true)}>Show</button>{" "}
+          </p>
+        </div>
+      );
+    }
+  };
+  return displayCard();
+};
 
 export default BlogCard;
